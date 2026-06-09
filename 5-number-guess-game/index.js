705d241f5ba8f41fdb1d151adbe9a0f3db241108
Nguyen Van Hau <hau551022@gmail.com>
2026-06-09 22:10:35 +0700

    program

--- 5-number-guess-game/index.js
+++ 5-number-guess-game/index.js
@@ -6,19 +6,20 @@ const highscore = document.getElementById("highscore");
 
 let minNum = 1;
 let maxNum = 100;
-let answer = Math.floor(Math.random() * (maxNum - minNum + 1));
+let answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
 let score = 10;
 let high = 0;
 
 check_btn.onclick = function () {
   const guess_input = +document.getElementById("guess-input").value;
+
   if (
     isNaN(guess_input) ||
     document.getElementById("guess-input").value === ""
   ) {
-    message.textContent = "please  enter a valid number";
+    message.textContent = "Please enter a valid number";
   } else if (guess_input < minNum || guess_input > maxNum) {
-    message.textContent = "please  enter a valid number";
+    message.textContent = "Please enter a valid number";
   } else if (guess_input < answer) {
     score--;
     displayScore.textContent = score;
@@ -42,14 +43,16 @@ check_btn.onclick = function () {
       high = score;
     }
     highscore.textContent = high;
-    message.textContent = "Correct";
+    message.textContent = "Correct! You win!";
+    check_btn.disabled = true;
   }
 };
 
 resetBtn.onclick = function () {
   answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
-  message.textContent = "";
+  message.textContent = "Waiting for your guess...";
   score = 10;
   displayScore.textContent = score;
   document.getElementById("guess-input").value = "";
+  check_btn.disabled = false;
 };
